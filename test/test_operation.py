4ff5eff8371b58998e24514fb137335e31d6cede
Komal Mandal <komalmandal1907@gmail.com>
2025-06-12 11:26:58 +0530

    done

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,16 +1,16 @@
 
-from src.math_operation import add,sub
 import unittest
+from src.math_operation import add, sub
 
-def test_add():
-    assert(2,3) == 5
-    assert(-1,1) == 0
+class TestMathOperation(unittest.TestCase):
 
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
 
-def test_sub():
-    assert(5,3) == 2
-    assert(2,2) == 0
-    assert(-1,1) == -2
+    def test_sub(self):
+        self.assertEqual(sub(2, 2), 0)
+        self.assertEqual(sub(-1, 1), -2)
 
 if __name__ == '__main__':
-        unittest.main()  
+    unittest.main()
+  
